eda526b723738fb1a49d74cc9585af442a31ddfa
Evan Laube <laubeevan@gmail.com>
2025-03-26 11:10:00 -0500

    Improve error checking to prompt for input again on invalid input

--- temperature_main.c
+++ temperature_main.c
@@ -4,61 +4,69 @@
 
 int main() {
 
-    int input;
+    float input;
     char unit;
     char convertTo;
 
+    float celsius;
+    float fahrenheit;
+    float kelvin = -1;
 
-    printf("Enter temperature to convert (exclude unit suffix): ");
-    scanf("%d", &input);
+    do {
+        printf("Enter temperature to convert (exclude unit suffix): ");
+        scanf("%f", &input);
 
-    printf("Enter input unit as a single character ('f', 'c', or 'k'): ");
-    scanf(" %c", &unit);
+        printf("Enter input unit as a single character ('f', 'c', or 'k'): ");
+        scanf(" %c", &unit);
 
-    // Ensure that the input unit is always a single lowercase character
-    unit = tolower(unit);
+        // Ensure that the input unit is always a single lowercase character
+        unit = tolower(unit);
 
-    if(unit != 'c' && unit != 'f' && unit != 'k') {
-        printf("ERROR: Invalid unit input, use 'f', 'c', or 'k' only\n");
-        return 1;
-    }
+        if(unit != 'c' && unit != 'f' && unit != 'k') {
+            printf("ERROR: Invalid unit input, use 'f', 'c', or 'k' only\n");
+            continue;
+        }
 
-    int celsius;
-    int fahrenheit;
-    int kelvin;
+        if(unit == 'c') {
+            celsius = input;
+        } else if (unit == 'f'){
+            celsius = fahrenheit_to_celsius(input);
+        } else {
+            celsius = kelvin_to_celsius(input);
+        }
+        fahrenheit = celsius_to_fahrenheit(celsius);
+        kelvin = celsius_to_kelvin(celsius);
 
-    if(unit == 'c') {
-        celsius = input;
-    } else if (unit == 'f'){
-        celsius = fahrenheit_to_celsius(input);
-    } else {
-        celsius = kelvin_to_celsius(input);
-    }
-    fahrenheit = celsius_to_fahrenheit(celsius);
-    kelvin = celsius_to_kelvin(celsius);
+        if(kelvin < 0) {
+            printf("ERROR: Temperature cannot be less than aboslute zero\n");
+            continue;
+        }
+    } while(kelvin < 0);
 
-    printf("Enter unit to convert to in same format: ");
-    scanf(" %c", &convertTo);
+    do {
+        printf("Enter unit to convert to in same format: ");
+        scanf(" %c", &convertTo);
 
-    // Ensure that the unit to convert to is always a single lowercase character
-    convertTo = tolower(convertTo);
+        // Ensure that the unit to convert to is always a single lowercase character
+        convertTo = tolower(convertTo);
 
-    if(convertTo != 'c' && convertTo != 'f' && convertTo != 'k') {
-        printf("ERROR: Invalid unit input, use 'f', 'c', or 'k' only\n");
-        return 1;
-    }
+        if(convertTo != 'c' && convertTo != 'f' && convertTo != 'k') {
+            printf("ERROR: Invalid unit input, use 'f', 'c', or 'k' only\n");
+            continue;
+        }
 
-    if(convertTo == unit) {
-        printf("ERROR: Cannot convert to same unit\n");
-        return 1;
-    }
+        if(convertTo == unit) {
+            printf("ERROR: Cannot convert to same unit\n");
+            continue;
+        }
+    } while (convertTo != 'c' && convertTo != 'f' && convertTo != 'k'); 
 
     if(convertTo == 'c') {
-        printf("Converted temperature: %d C\n", celsius);
+        printf("Converted temperature: %f C\n", celsius);
     } else if(convertTo == 'f') {
-        printf("Converted temperature: %d F\n", fahrenheit);
+        printf("Converted temperature: %f F\n", fahrenheit);
     } else {
-        printf("Converted temperature: %d K\n", kelvin);
+        printf("Converted temperature: %f K\n", kelvin);
     }
 
     categorize_temperature(celsius);
