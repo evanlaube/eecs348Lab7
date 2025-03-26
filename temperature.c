8bab2bde2643b74c93f12265171d88f81233a2a9
Evan Laube <laubeevan@gmail.com>
2025-03-26 03:15:26 -0500

    Add skeletons of temp functions

--- temperature.c
+++ temperature.c
@@ -1,1 +1,18 @@
 #include "temperature.h"
+
+
+float celsius_to_fahrenheit(float celsius) {
+    return 0;
+}
+
+float fahrenheit_to_celsius(float fahrenheit) {
+    return 0;
+}
+
+float celsius_to_kelvin(float celsius) {
+    return 0;
+}
+
+float kelvin_to_celsius(float celsius) {
+    return 0;
+}
