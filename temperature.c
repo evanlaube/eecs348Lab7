607eb71cd13a5f19431156b00709622f3786dbf6
Evan Laube <laubeevan@gmail.com>
2025-03-26 11:09:40 -0500

    Add categorization of temperatures

--- temperature.c
+++ temperature.c
@@ -2,7 +2,22 @@
 #include <stdio.h>
 
 void categorize_temperature(float celsius) {
-    printf("Categorizing temperature...\n");
+    if(celsius < 0) {
+        printf("Current condition: Freezing\n");
+        printf("Advisory: Put on a heavy coat, hat, and gloves. Limit time outside\n");
+    } else if (celsius < 10) {
+        printf("Current condition: Cold\n");
+        printf("Advisory: A heavy jacket should be fine\n");
+    } else if (celsius < 25) {
+        printf("Current condition: Comfortable\n");
+        printf("Advisory: Wear comfortable light clothes. Enjoy the nice weather!\n");
+    } else if (celsius < 35) {
+        printf("Current condition: Hot\n");
+        printf("Advisory: Wear light clothes and be aware of overheating.\n");
+    } else {
+        printf("Current condition: Extreme heat\n");
+        printf("Advisory: It's too hot to be outside safely. Stay indoors.\n");
+    }
 }
 
 float celsius_to_fahrenheit(float celsius) {
